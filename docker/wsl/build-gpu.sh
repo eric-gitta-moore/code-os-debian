5189d83b573f298a3fc44d169342ac815929d87b
Eric Moore <EricGittaMoore@duck.com>
2023-08-20 20:34:49 +0800

    fix: build.sh

--- docker/wsl/build-gpu.sh
+++ docker/wsl/build-gpu.sh
@@ -1,2 +1,2 @@
 source .envrc
-docker compose build --build-arg IMAGE_NAME=tensorflow/tensorflow:latest-gpu
+docker compose build --build-arg IMAGE_NAME=tensorflow/tensorflow:latest-gpu --build-arg GPU_SUPPORT=on
